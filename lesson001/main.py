939051aee5511ecde5ba674bc2af36aa4a2a7ce8
JMatveichik <johnny.matveichik@gmail.com>
2025-03-21 17:39:36 +0300

    Train console input output operations

---
+++ lesson001/main.py
@@ -0,0 +1,12 @@
+print('This my first training lesson')
+
+num1 = float(input('Введите первое число : '))
+num2 = float(input('Введите второе число : '))
+
+sep = '+'*30
+print(sep)
+
+print(f'Сумма {num1} + {num2} =', num1 + num2)
+print(f'Произведение {num1} * {num2} =', num1 * num2)
+
+print(sep)
